677fa17b376c915f5f8635c5c9f47bda0628b262
Piet De Vaere <piet@devae.re>
2015-07-08 15:03:48 +0200

    with login page

--- webapp.py
+++ webapp.py
@@ -10,67 +10,11 @@
 
 from datetime import datetime
 from random import random
-from flask import Flask, render_template, request
+from flask import Flask, url_for, redirect, render_template, request
+from os import listdir
 app = Flask(__name__)
 
-aantal_totems = 396  
-
-
-## User feedback geven, en logfile aanvullen
-def give_feedback(logfile, frequenties_gesorteerd):
-    print("")
-    logfile.write("---------------------------------------\n")
-    print("{0:6} {1:9} {2}".format("Nummer", "Abs freq", "Rel freq (%)"))
-    logfile.write("{0:6} {1:9} {2}\n".format("Nummer", "Abs freq", "Rel freq (%)"))
-    for nummer in frequenties_gesorteerd:
-        if nummer[1] > 0:
-            print("{0:6} {1:9} {2}".format(str(nummer[0]), str(nummer[1]),str(nummer[3])))
-            logfile.write("{0:6} {1:9} {2}\n".format(str(nummer[0]), str(nummer[1]),str(nummer[3])))
-    print("{0:6} {1:9} {2}".format("Nummer", "Abs freq", "Rel freq (%)"))
-
-def user_feedback(adjectief):
-    """vraagt de gebruiker of een adjectief van toepassing is"""
-    for i in range(5):
-        respons = str(input("{}? ".format(adjectief)))
-        if respons in {"yes", "y", "ja", "j", "1"}:
-            return True
-        elif respons in {"no", "n", "nee", "0"}:
-            return False
-        elif respons in {"quit", "q"}:
-            exit()
-        else:
-            print("antwoorden met 'j','ja', 'n', 'nee', '0' of '1'")
-
-
-        
-def read_file(naam):
-    answers = {}
-
-    ## first skip through the first lines of the adjectievenfile
-    ## coppy the first part of the logfile to the new logfile
-    try:
-        inputfile = open("./logs/{}.txt".format(naam), 'r') 
-        inputlines = inputfile.readlines()
-        inputfile.close()
-    except FileNotFoundError:
-        print("No previous test of {} found, starting new test".format(naam))
-        return {}
-    
-    logfile = open("./logs/{}.txt".format(naam), 'w')  
-    for line in inputlines:
-        if line[0] == "-":
-            break
-  ##    logfile.write(line)
-        line = line.strip()
-        adjectief, answer = line.split(":")[0:2]
-        answer = int(answer.strip())
-        answers[adjectief] = answer
-    
-    return answers
-
 def read_adjectives(inputFile):            
-##  logfile = open("./logs/{}_{}.txt".format(naam, str(datetime.now())), 'w')
-    ## Door alle adjectieven loopen
     adjectives = {}
     for line in open(inputFile, 'r'):
         ## Kijken of de lijn een opmerking is
@@ -89,13 +33,11 @@ def read_adjectives(inputFile):
             adjectives[adjective] = numbers
     return adjectives
 
-
 def process_answers(answers):
     for adjective in adjectives.keys():
         if adjective in answers:
             for number in adjectives[adjective]:
                 frequencies[number][1] += 1
-    
 
 def parse_questionaire():
     print(request.form)
@@ -120,18 +62,16 @@ def calc_relative_freq():
     return freq_sorted
 
 def writeout(answers, name):
-    path = 'results/'
-    f = open(path+name, 'w')
+    f = open(result_path+name, 'w')
     for answer in answers:
         f.write(answer+'\n')
 
 def readin(name = None):
-    path = 'results/'
     answers = set()
     if name == None:
         return anwers
     try:
-        f = open(path+name)
+        f = open(result_path+name)
     except:
         return answers
     for line in f:
@@ -139,10 +79,21 @@ def readin(name = None):
         answers.add(line)
     return answers
 
+def list_users():
+    return listdir(result_path)
 
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        users = list_users()
+        return render_template('login.html', users=users)
+
+    if request.method == 'POST':
+        return redirect(url_for('userpage', username=request.form['username']))
+    
 
 @app.route('/<username>', methods=['POST', 'GET'])
-def index(username):
+def userpage(username):
     if request.method == 'POST':
         answers = parse_questionaire()
         process_answers(answers)
@@ -156,32 +107,12 @@ def index(username):
 
 ## Adjectievenlijst aanmaken
 ## formaat: [nummer, absolute frequentie, maximum, relative frequentie]
-frequencies = [[i,0,0,0] for i in range(aantal_totems+1)]
+number_of_totems = 396  
+frequencies = [[i,0,0,0] for i in range(number_of_totems+1)]
+result_path = 'results/'
+
 
 adjectives = read_adjectives('adjectievenlijst.txt')
 sorted_adjectives = sorted(adjectives.keys())
 app.debug = True
 app.run()
-
-## check what program to exexute    
-##valid = 0
-##while valid == 0:
-##    task = str(input("[N]ew test, or [o]pen and old file? "))
-##    if task in ("new", "NEW", "New", "N", "n", ""):
-##        ## gebruiker vragen om naam
-##        naam = str(input("Echte naam van de totemloze: "))
-##        proces_list({}, naam)
-##        valid = 1
-##
-##    elif task in ("open", "OPEN", "Open", "O", "o"):
-##        ## gebruiker vragen om naam
-##        naam = str(input("Echte naam van de totemloze: "))
-##        answers = read_file(naam)
-##        proces_list(answers, naam)
-##        valid = 1
-##
-##    elif task in ("quit", "exit", "q"):
-##        exit()
-##
-##input("Druk enter om af te sluiten")
-
